80df901dc33012b2f878840369737323922d28be
Ashutoshk2002 <90946675+Ashutoshk2002@users.noreply.github.com>
2023-01-22 21:40:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function publishToTable() {
+    const name = document.getElementById('exampleInputName').value;
+    const email = document.getElementById('exampleInputEmail').value;
+    const phone = document.getElementById('exampleInputPhoneno').value;
+    const address = document.getElementById('exampleInputAddress').value;
+    const error = document.getElementById('error');
+    event.preventDefault();
+
+    error.innerHTML = (!name || !email || !phone || !address) ? 'Name, Email, Phone Number, Address values are required.' : '';
+    if (name && email && phone && address) {
+        const tableElement = document.getElementById('table');
+        const trElement = document.createElement('tr');
+        const tbodyElement = document.createElement('tbody');
+        const nameEle = document.createElement('td');
+        const emailEle = document.createElement('td');
+        const phoneEle = document.createElement('td');
+        const AddEle = document.createElement('td');
+        nameEle.innerHTML = name;
+        emailEle.innerHTML = email;
+        phoneEle.innerHTML = phone;
+        AddEle.innerHTML = address;
+        trElement.appendChild(nameEle);
+        trElement.appendChild(emailEle);
+        trElement.appendChild(phoneEle);
+        trElement.appendChild(AddEle);
+        tbodyElement.appendChild(trElement);
+        tableElement.appendChild(tbodyElement);
+    }
+}
